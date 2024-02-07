4f4735eba1d32ed8b09e7a1b04b317041abfa295
Torsten Sprenger <mail@spren9er.de>
2024-02-07 18:06:49 +0100

    Bump packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: preprocess(),
 
   kit: {
     adapter: adapter(),
